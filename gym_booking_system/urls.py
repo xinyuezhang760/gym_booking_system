a333ff57225f7a99e295ce7209af465b370be061
Xinyue Zhang <2965498z@student.gla.ac.uk>
2025-08-01 21:08:16 +0800

    Added media static serving in urls.py

--- gym_booking_system/urls.py
+++ gym_booking_system/urls.py
@@ -28,7 +28,4 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-from django.conf import settings
-from django.conf.urls.static import static
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
